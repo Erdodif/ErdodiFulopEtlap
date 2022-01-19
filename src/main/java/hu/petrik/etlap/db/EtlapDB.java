8e0fe990db5c0e193c937e71b8634da892fd8453
Erdodif <erdodif@gmail.com>
2022-01-19 16:40:30 +0100

    Áremelés megvalósítása

--- src/main/java/hu/petrik/etlap/db/EtlapDB.java
+++ src/main/java/hu/petrik/etlap/db/EtlapDB.java
@@ -1,4 +1,7 @@
 package hu.petrik.etlap.db;
+
+import javafx.fxml.FXML;
+
 import java.sql.*;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -8,7 +11,7 @@ public class EtlapDB {
     Connection conn;
 
     public EtlapDB() throws SQLException {
-        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/etlapdb","root","");
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/etlapdb", "root", "");
     }
 
     public List<Etel> getEtelek() throws SQLException {
@@ -16,7 +19,7 @@ public class EtlapDB {
         Statement stmt = conn.createStatement();
         String sql = "SELECT * FROM etlap;";
         ResultSet result = stmt.executeQuery(sql);
-        while (result.next()){
+        while (result.next()) {
             etelek.add(new Etel(
                     result.getInt("id"),
                     result.getString("nev"),
@@ -28,31 +31,63 @@ public class EtlapDB {
         return etelek;
     }
 
-    public boolean addEtel(Etel etel) throws  SQLException{
+    public boolean addEtel(Etel etel) throws SQLException {
         String sql = "INSERT INTO `etlap` (nev, leiras, ar, kategoria_id) VALUES(?,?,?,?)";
         PreparedStatement prpstm = this.conn.prepareCall(sql);
-        prpstm.setString(1,etel.getNev());
-        prpstm.setString(2,etel.getLeiras());
-        prpstm.setInt(3,etel.getAr());
-        prpstm.setInt(4,etel.getKategoria().getId());
+        prpstm.setString(1, etel.getNev());
+        prpstm.setString(2, etel.getLeiras());
+        prpstm.setInt(3, etel.getAr());
+        prpstm.setInt(4, etel.getKategoria().getId());
         return prpstm.executeUpdate() == 1;
     }
 
-    public boolean deleteEtel(int id) throws SQLException{
+    public boolean deleteEtel(int id) throws SQLException {
         String sql = "DELETE FROM `filmek` WHERE id = ?";
         PreparedStatement prpstmt = conn.prepareStatement(sql);
-        prpstmt.setInt(1,id);
+        prpstmt.setInt(1, id);
         return prpstmt.executeUpdate() == 1;
     }
 
-    public boolean editEtel(Etel etel) throws SQLException{
+    public boolean editEtel(Etel etel) throws SQLException {
         String sql = "UPDATE `etlap` SET nev = ?, leiras = ?, ar = ?, kategoria_id = ? WHERE id = ?;";
         PreparedStatement prpstm = this.conn.prepareCall(sql);
-        prpstm.setString(1,etel.getNev());
-        prpstm.setString(2,etel.getLeiras());
-        prpstm.setInt(3,etel.getAr());
-        prpstm.setInt(4,etel.getKategoria().getId());
-        prpstm.setInt(5,etel.getId());
+        prpstm.setString(1, etel.getNev());
+        prpstm.setString(2, etel.getLeiras());
+        prpstm.setInt(3, etel.getAr());
+        prpstm.setInt(4, etel.getKategoria().getId());
+        prpstm.setInt(5, etel.getId());
         return prpstm.executeUpdate() == 1;
     }
+
+    public boolean raisePrice(int modifier, Integer etelID, boolean szazalek) throws SQLException {
+        String whereIfSingle = etelID == null ? "WHERE id = ?" : "";
+        if(szazalek){
+            if (modifier < 5) {
+                throw new IllegalArgumentException("Áremelés nem lehet 5% alatt!");
+            }
+            if (modifier > 50){
+                throw new IllegalArgumentException("Áremelés nem lehet 50% felett!");
+            }
+        }
+        else{
+            if (modifier < 50) {
+                throw new IllegalArgumentException("Áremelés nem lehet 50Ft alatt!");
+            }
+            if (modifier > 3000) {
+                throw new IllegalArgumentException("Áremelés nem lehet 3000Ft felett!");
+            }
+        }
+        String sql = "UPDATE `etlap` SET ar = ar * (1 + (100 / ?)) " + whereIfSingle + ";";
+        if (modifier > 50) {
+            sql = "UPDATE `etlap` SET ar = ar + ? " + whereIfSingle + ";";
+        }
+        PreparedStatement prpstm = this.conn.prepareCall(sql);
+        prpstm.setInt(1, modifier);
+        if(etelID != null){
+            prpstm.setInt(2, etelID);
+        }
+        return prpstm.executeUpdate() == 1;
+    }
+
+
 }
