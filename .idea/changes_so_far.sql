42485e217591baf9912b37f26bd0fde4646f98bd
Erdodif <erdodif@gmail.com>
2022-01-27 15:49:44 +0100

    javítás

--- .idea/changes_so_far.sql
+++ .idea/changes_so_far.sql
@@ -2,11 +2,11 @@ CREATE TABLE `kategoria` (`id` INT NOT NULL AUTO_INCREMENT PRYMARY KEY,`nev` VAR
 
 INSERT INTO kategoria(`nev`) SELECT DISTINCT `etlap`.`kategoria` FROM `etlap`;
 
-ALTER TABLE `etlap` ADD COLUMN `kategoria_id` INT NOT NULL
+ALTER TABLE `etlap` ADD COLUMN `kategoria_id` INT NOT NULL;
 
 UPDATE `etlap`
 JOIN `kategoria`
 ON `kategoria`.`nev` = `etlap`.`kategoria`
 SET `etlap`.`kategoria_id` = `kategoria`.`id`;
 
-ALTER TABLE `etlap` DROP COLUMN `kategoria`
+ALTER TABLE `etlap` DROP COLUMN `kategoria`;
